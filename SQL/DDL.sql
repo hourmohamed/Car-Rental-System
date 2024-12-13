601cd05726d2c003205cc94f03a193b20ba2e2f7
NadineTamish <nadineahmed81@gmail.com>
2024-12-13 22:50:10 +0200

    rental and employee table

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -17,5 +17,34 @@ CREATE TABLE customer(
     customer_name varchar(50),
     phone_number INT
     );
-    
+
+
+CREATE TABLE rental(
+    rental_id INT PRIMARY KEY,
+    customer_id INT,
+    rental_date DATE,
+    return_date DATE,
+    car_id INT
+
+
+);
+
+CREATE TABLE employee(
+    employee_id INT PRIMARY KEY,
+    office_number INT,
+    `name` varchar(10),
+    mgr_id INT
+
+    date
+
+
+);
+
+-- for foreign key 
+ALTER TABLE rental
+ADD FOREIGN KEY (car_id) REFERENCES car (car_id),
+ADD FOREIGN KEY (customer_id) REFERENCES customer(customer_id);
+
+
+
     DDL.sql
