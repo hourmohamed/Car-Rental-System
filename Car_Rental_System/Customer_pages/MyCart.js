f688e80ec5e5afceaee0e83cb3c1f42d939f4ef5
NadineTamish <nadineahmed81@gmail.com>
2024-12-23 16:00:03 +0200

    serach and reservation

---
+++ Car_Rental_System/Customer_pages/MyCart.js
@@ -0,0 +1,26 @@
+// Function to add car to the reservation cart
+function addToCart(carId, carModel, carPrice) {
+    const cart = JSON.parse(localStorage.getItem('reservationCart')) || [];
+    
+    // Check if the car is already in the cart
+    const existingCar = cart.find(item => item.carId === carId);
+    
+    if (existingCar) {
+      // If the car is already in the cart, just increase the quantity
+      existingCar.quantity++;
+    } else {
+      // If the car is not in the cart, add a new entry
+      cart.push({ carId, carModel, carPrice, quantity: 1 });
+    }
+    
+    // Save the updated cart back to localStorage
+    localStorage.setItem('reservationCart', JSON.stringify(cart));
+  
+    alert(`${carModel} added to your cart!`);
+  }
+  
+  // Function to view the cart
+  function viewCart() {
+    window.location.href = 'MyCart.html'; // Redirect to the cart page
+  }
+  
