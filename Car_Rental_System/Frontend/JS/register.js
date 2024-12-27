a6bd7fd3ad3df8c161ded9cfd5422a4a9dfaee6f
unknown <hour.mohamed01@gmail.com>
2024-12-27 09:02:53 +0200

    organized and admin redirection done

---
+++ Car_Rental_System/Frontend/JS/register.js
@@ -0,0 +1,31 @@
+// Check if the user is already registered when accessing the registration page
+const registeredUsers = JSON.parse(localStorage.getItem('registeredUsers')) || [];
+
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const phone = document.getElementById('phone').value;
+
+  // Check if the email is already registered
+  const userExists = registeredUsers.some(user => user.email === email);
+
+  if (userExists) {
+    alert('This email is already registered. Please log in!');
+    window.location.href = '../HTML/customer_login.html'; // Redirect to login page if email is already registered
+    return;
+  }
+
+  // Register new user by adding them to the registered users array
+  const newUser = { firstName, lastName, email, password, phone };
+  registeredUsers.push(newUser);
+
+  // Save the updated users array to localStorage
+  localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers));
+
+  alert('Registration successful! Redirecting to the Login page...');
+  window.location.href = '../HTML/customer_login.html'; // Redirect to home page after successful registration
+});
