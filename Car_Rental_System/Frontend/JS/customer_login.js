4541bae6e388546051c9c4c189973af29f08cb0c
NadineTamish <nadineahmed81@gmail.com>
2024-12-29 19:14:20 +0200

    invalid password in customer login is fixed, added navigation bar  to customer serach, buttom at Mycart to return to serach page

--- Car_Rental_System/Frontend/JS/customer_login.js
+++ Car_Rental_System/Frontend/JS/customer_login.js
@@ -1,24 +1,17 @@
 function validateForm() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  passed = true
 
-  
   if (email === "") {
-      alert("Email cannot be empty");
-      passed = false;
-      return false; 
+    alert("Email cannot be empty");
+    return false; // Prevent form submission
   }
 
   if (password === "") {
-      alert("Password cannot be empty");
-      passed = true;
-      return false; 
-    }
-    if (passed) {
-      // Redirect to customer_search page after successful validation
-      window.location.href = "../../Frontend/HTML/customer_search.html"; // Adjust the URL as needed
-    }
-  return passed;
-}
+    alert("Password cannot be empty");
+    return false; // Prevent form submission
+  }
 
+  // Allow form submission to the PHP script
+  return true;
+}
